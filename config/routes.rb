1f729961d61e82a3e091cd4e33b603284796dca0
小坂竜也 <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2022-11-12 14:43:37 +0900

    sign_up show indexのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root "users#home"
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+    }
+
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
