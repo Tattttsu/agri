fadc3795b838abb0ee5aea9ad452b83ce2cdb01b
小坂竜也 <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2022-11-13 13:04:48 +0900

    フォロー機能のルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations'
     }
 
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index] do
+    resource :relationships, only: [:create, :destroy]
+    get :followings, on: :member
+    get :followers, on: :member
+  end
+
+  resources :farms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
