089ef44563491a5d204edd3565faf811d59088b6
小坂竜也 <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2022-11-23 11:55:40 +0900

    mergeのコンフリクトの解消

--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,7 +2,7 @@ class Farm < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
-  
+
   validates :user_id, presence: true
   validates :name, presence: true
 end
