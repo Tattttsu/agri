6e5fe2c0eaad1628531b83bbc1fdc25f34ddc3d9
小坂竜也 <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2022-12-11 16:44:27 +0900

    farmのshowとindexのビューに画像が表示されるように

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   end
 
   resources :farms do
-    resource :likes, only: [:create, :destroy]
+    resource :likes, only: [:create, :destroy, :edit]
     resources :comments, only: [:edit, :update, :create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
