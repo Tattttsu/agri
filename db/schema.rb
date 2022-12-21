7651ae1597dc3d89d7d7046c8ae3ac2e7b6468dc
小坂竜也 <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2022-12-21 22:18:32 +0900

    userの画像を表示するよう修正”

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_14_122601) do
+ActiveRecord::Schema.define(version: 2022_12_21_124553) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2022_12_14_122601) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "icon"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
