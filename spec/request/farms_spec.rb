8f602edbcd8b2adbbafafda0926ef4c09921e554
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-02-15 21:29:40 +0900

    farmのrequest specの作成

--- spec/request/farms_spec.rb
+++ spec/request/farms_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Farms", type: :request do
+  let(:user) { create(:user) }
+  let(:farm) { create(:farm, user_id: user.id) }
 
-  context 'Farms' do
-    it '新規作成画面' do
+  describe 'GET /new' do
+    it 'リクエストが成功すること' do
+      sign_in user
       get new_farm_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /index' do
+    it 'リクエストが成功すること' do
+      get farms_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /edit' do
+    before do
+      sign_in user
+      get edit_farm_path(farm.id)
+    end
+    it 'リクエストが成功すること' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'farm名が表示されている' do
+      expect(response.body).to include farm.name
     end
   end
 end
