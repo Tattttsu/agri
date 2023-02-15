988b256c4905f8265b1593c033f4157fc7ed9e33
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-02-15 21:43:38 +0900

    userのmodel specの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = build(:user)
+  end
+
+  describe 'バリデーション' do
+    it 'nameが空だとNG' do
+      @user.name = ''
+      expect(@user.valid?).to eq(false)
+    end
+
+    it 'emailが空だとNG' do
+      @user.email = ''
+      expect(@user.valid?).to eq(false)
+    end
+
+    it 'nameが空だとNG' do
+      @user.password = ''
+      expect(@user.valid?).to eq(false)
+    end
+  end
+  describe 'アソシエーションのテスト' do
+    context 'farmモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:farms).macro).to eq :has_many
+      end
+    end
+  end
 end
