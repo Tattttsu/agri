00a6f4e664a955b97cc24919cfae73f21688a47c
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-03-12 15:05:14 +0900

    rubocopの修正

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,13 @@ class Ability
   include CanCan::Ability
   def initialize(user)
     user ||= User.new
-    if user.role == 2
-      can :manage, :all
+    can :manage, :all
+    case user.role
+    when 2
       can :access_admin_page, :all
-    elsif user.role == 1
-      can :manage, :all
+    when 1
       cannot :access_admin_page, :all
     else
-      can :manage, :all
       cannot :create, :all
       cannot :access_admin_page, :all
     end
