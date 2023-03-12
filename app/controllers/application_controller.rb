36a3bca9aeb0870da22cfa6ae90e33120eec40d6
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-03-12 13:54:54 +0900

    テストアカウントを編集したらログインできなくなるエラーを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |_exception|
-    redirect_to root_path, alert: '「農家の方はこちら」から登録してください。'
+    redirect_to root_path, alert: 'マイページより農家として登録してください。'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2023_03_12_020701) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "icon"
-    t.integer "role", default: 3
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
