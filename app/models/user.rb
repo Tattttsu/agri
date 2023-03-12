2385d6c1309bee5659bccffe30303d5f8b2a7f17
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-03-12 20:51:41 +0900

    メッセージ機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
 
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+  has_many :entries, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,8 @@ Rails.application.routes.draw do
     resource :likes, only: [:create, :destroy, :edit]
     resources :comments, only: [:edit, :update, :create, :destroy]
   end
+
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
