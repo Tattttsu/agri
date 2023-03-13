a20ff057474b09359e6f52f052f091b1bcc4f636
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-03-13 23:18:23 +0900

    メッセージ機能を作成し、非同期通信に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,20 @@ class UsersController < ApplicationController
     @farms = @user.farms
     @current_entry = Entry.where(user_id: current_user.id)
     @another_entry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @current_entry.each do |current|
-        @another_entry.each do |another|
-          if current.room_id == another.room_id
-            @is_room = true
-            @room_id = current.room_id
-          end
+    return if @user.id == current_user.id
+
+    @current_entry.each do |current|
+      @another_entry.each do |another|
+        if current.room_id == another.room_id
+          @is_room = true
+          @room_id = current.room_id
         end
       end
-      unless @is_room
-        @room = Room.new
-        @entry = Entry.new
-      end
     end
+    return if @is_room
+
+    @room = Room.new
+    @entry = Entry.new
   end
 
   def index
