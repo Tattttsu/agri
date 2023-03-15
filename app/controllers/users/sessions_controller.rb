9305c41df1a99069d4c29d9f5c9611aac1580b00
たつや <kosakatatsuya@miyamotoayuminoMacBook-Air.local>
2023-03-15 20:54:05 +0900

    テストログインのユーザーがずれていたので修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,12 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
   def new_guest
-    user = User.find(1)
+    user = User.find(2)
     sign_in user
     redirect_to root_path, notice: "テストログイン"
   end
 
   def new_guest_farmer
-    user = User.find(2)
+    user = User.find(3)
     sign_in user
     redirect_to root_path, notice: "農家としてテストログイン"
   end
